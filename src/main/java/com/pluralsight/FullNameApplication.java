6ba6577ba26c7dabf7bcd6d7987b186585f755b8
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-13 13:15:32 -0400

    Initial Commit. Finished code but can't get it recongize middleName string.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -8,5 +8,29 @@ import java.util.regex.Pattern;
 public class FullNameApplication {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String hasMiddleName = "None";
+
+        if (middleName.equalsIgnoreCase((hasMiddleName))) {
+            System.out.println("");
+        } else {
+            System.out.println(middleName);
+        }
+
+        System.out.print("What is your first name? ");
+        String firstName = scan.nextLine();
+
+        System.out.println("What is your middle name , if you do not have one type none");
+        String middleName = scan.nextLine();
+
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+
+        System.out.println("If you have a suffix, please add it: ");
+        String suffix = scan.nextLine();
+
+        String fullName = suffix + firstName + middleName + lastName;
+        System.out.println(suffix  + ", " + fullName);
+
     }
 }
