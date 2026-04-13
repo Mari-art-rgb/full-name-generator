ecb54b367174192955960677472e9b275d6467f0
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-13 11:20:14 -0400

    Initial Commit.

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+import java.util.regex.Pattern;
+
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
