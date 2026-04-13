1dd3f74006072663aa916adcd17530dc1b1c3783
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-13 13:32:25 -0400

    Initial Commit. Finished code but can't get it recongize middleName string.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -9,13 +9,10 @@ public class FullNameApplication {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String hasMiddleName = "None";
+        String hasMiddleName = "";
+        String hasSuffix = "";
 
-        if (middleName.equalsIgnoreCase((hasMiddleName))) {
-            System.out.println("");
-        } else {
-            System.out.println(middleName);
-        }
+
 
         System.out.print("What is your first name? ");
         String firstName = scan.nextLine();
@@ -23,14 +20,26 @@ public class FullNameApplication {
         System.out.println("What is your middle name , if you do not have one type none");
         String middleName = scan.nextLine();
 
+        if (middleName.equalsIgnoreCase((hasMiddleName))) {
+            System.out.println("");
+        } else {
+            System.out.println(middleName);
+        }
+
         System.out.println("What is your last name?");
         String lastName = scan.nextLine();
 
         System.out.println("If you have a suffix, please add it: ");
         String suffix = scan.nextLine();
 
+        if (suffix.equalsIgnoreCase((hasSuffix))) {
+            System.out.println("");
+        } else {
+            System.out.println(suffix);
+        }
+
         String fullName = suffix + firstName + middleName + lastName;
-        System.out.println(suffix  + ", " + fullName);
+        System.out.println(fullName + ", " + suffix);
 
     }
 }
